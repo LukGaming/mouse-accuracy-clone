9de5d705cffbc357744458421d6d4580b84eb855
Paulo Antonio Mendes <thelukgaming333@gmail.com>
2022-04-02 16:20:54 -0300

    Arrumando bug que trava animacao

--- src/views/Canvas.vue
+++ src/views/Canvas.vue
@@ -1,6 +1,9 @@
 <template >
     <div class="bg-page">
         <v-stage :config="configKonva">
+            <v-layer>
+                <v-text ref="pontuacao" :config="pontuacaoConfig"></v-text>
+            </v-layer>
             <v-layer ref="circles" v-for="(circle, index) in circles" :key="index">
                 <v-circle :config="circle.configCircle" @click="clickingCircle(index)"></v-circle>
             </v-layer>
@@ -14,10 +17,19 @@ const heigth = window.innerHeight
 export default {
     data() {
         return {
+            // pontuacao: 0,
             configKonva: {
                 width: width,
                 height: heigth,
                 fill: 'red',
+            },
+            pontuacaoConfig: {
+                x: width - 200,
+                y: 15,
+                text: 0,
+                fontSize: 30,
+                fontFamily: 'Calibri',
+                fill: 'green',
             },
             // configCircle: {
             //     x: 100,
@@ -27,82 +39,85 @@ export default {
             //     // stroke: "black",
             //     strokeWidth: 4
             // }
-            circles: []
+            circles: [],
+
         };
     },
     created() {
         var i = 0;
-        var speed = 1000;
+        var speed = 500;
         setInterval(() => {
-            i++;
-            this.circles.push({
-                configCircle: {
-                    x: this.setRandomRespawn(width),
-                    y: this.setRandomRespawn(heigth),
-                    radius: 5,
-                    fill: "red",
-                    // stroke: "black",
-                    strokeWidth: 4
-                }
-            })
-            this.growCircle(i)
+            // if (i < 5) {
+                this.circles.push({
+                    id: i,
+                    configCircle: {
+                        x: this.setRandomRespawn(width),
+                        y: this.setRandomRespawn(heigth),
+                        radius: 1,
+                        fill: "red",
+                        // stroke: "black",
+                        strokeWidth: 4
+                    }
+                })
+                this.growCircle(i)
+                i++;
+                console.log(this.circles)
+
+
+            // }
+
         }, speed);
 
     },
     methods: {
         clickingCircle(index) {
-            this.circles.splice(index, 1)
+            this.stopAnimation(index)
+            this.pontuacaoConfig.text++;
         },
         setRandomRespawn(parameter) {
             return Math.random() * parameter;
         },
+        stopAnimation(index) {
+            this.circles[index].animation.stop()
+            this.circles[index].layer.visible(false)
+            // this.circles[index].layer.setX(this.setRandomRespawn(width))
+            // this.circles[index].layer.setY(this.setRandomRespawn(heigth))
+        },
         growCircle(index) {
-
-            // this.circles[index].configCircle.radius = 200
-            // this.circles[index].configCircle.radius = 120
             setTimeout(() => {
                 if (this.circles[index]) {
-                    var layer = this.$refs.circles[index].getNode()
+                    this.circles[index].layer = this.$refs.circles[index].getNode()
                     this.circles[index].configCircle.lessThanMaximumRadius = false
-                    let anim = new Konva.Animation(() => {
+                    this.circles[index].animation = new Konva.Animation(() => {
                         // setInterval(() => {
-                        if (this.circles[index].configCircle.lessThanMaximumRadius == false) {
-                            this.circles[index].configCircle.radius = this.circles[index].configCircle.radius + 0.1
-                            if (this.circles[index].configCircle.radius >= 20) {
-                                this.circles[index].configCircle.lessThanMaximumRadius = true
+                        var radiusSpeed = 0.25
+                        if (this.circles[index]) {
+                            if (this.circles[index].configCircle.lessThanMaximumRadius == false) {
+                                this.circles[index].configCircle.radius = this.circles[index].configCircle.radius + radiusSpeed
+                                if (this.circles[index].configCircle.radius >= 20) {
+                                    this.circles[index].configCircle.lessThanMaximumRadius = true
+                                }
                             }
-                        }
-                        if (this.circles[index].configCircle.lessThanMaximumRadius == true) {
-                            if (this.circles[index].configCircle.radius >= 1) {
-                                this.circles[index].configCircle.radius = this.circles[index].configCircle.radius - 0.1
+                            if (this.circles[index].configCircle.lessThanMaximumRadius == true) {
+                                if (this.circles[index].configCircle.radius >= 1) {
+                                    this.circles[index].configCircle.radius = this.circles[index].configCircle.radius - radiusSpeed
+                                }
                             }
-                        }
-                        if (this.circles[index].configCircle.radius < 1) {
-                            this.circles.splice(index, 1)
+                            if (this.circles[index].configCircle.radius < 1) {
+                                // this.circles.splice(index, 1)
+                                this.stopAnimation(index);
+                                // this.circles[index].configCircle.lessThanMaximumRadius = true
+                                // this.circles[index].configCircle.radius = 2
+                            }
                         }
 
                         // }, 1000);
 
-                    }, layer)
-                    anim.start()
-                    // layer.to({
-                    //     innerRadius: 50,
-                    //     // scaleX: 50,
-                    //     // scaleY: Math.random() + 0.8,
-                    //     duration: 0.2
-                    // });
+                    }, this.circles[index].layer)
+                    this.circles[index].animation.start()
                     return Konva
                 }
             }, 1000);
-
-
-            // const anim = new Konva.Animation(function (frame) {
-            //     hexagon.setX(
-            //         amplitude * Math.sin((frame.time * 2 * Math.PI) / period) + centerX
-            //     );
-            // }, layer);
-
-
         }
     }
 
